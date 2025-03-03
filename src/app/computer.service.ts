65c30226cdc76aa7ee523322c455adab678cd495
Seleez <bartekczernek10@gmail.com>
2025-03-03 10:05:21 +0100

    computer list added and getComputer() created

--- src/app/computer.service.ts
+++ src/app/computer.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Computer } from './computer-list/computer-list.component';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,40 @@ import { Injectable } from '@angular/core';
 export class ComputerService {
 
   constructor() { }
+  computerList: Computer[] = [
+      {
+        processor: 'Intel I3-12200',
+        ram: 16,
+        GPU: 'RTX 4090',
+        Motherboard: 'Intel',
+      },
+      {
+        processor: 'Intel I5-12200',
+        ram: 32,
+        GPU: 'RTX 2060',
+        Motherboard: 'Intel',
+      },
+      {
+        processor: 'Intel I7-12200',
+        ram: 128,
+        GPU: 'RTX 4090',
+        Motherboard: 'Intel',
+      },
+      {
+        processor: 'Intel I9-12200',
+        ram: 64,
+        GPU: 'RTX 1030',
+        Motherboard: 'Intel',
+      },
+      {
+        processor: 'Intel I3-14020',
+        ram: 16,
+        GPU: 'RTX 3090',
+        Motherboard: 'Intel',
+      },
+    ];
+    
+    getComputers():   Observable<Computer[]>{
+      return of(this.computerList)
+    }
 }
